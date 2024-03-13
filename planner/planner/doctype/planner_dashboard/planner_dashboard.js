2121ef5ac9fec8da24f6d31ed0add1669fb87d7a
Christoph Diethelm <c.diethelm@diethelm-aufzuege.swiss>
2024-03-13 07:51:45 +0000

    doctype changes

---
+++ planner/planner/doctype/planner_dashboard/planner_dashboard.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ONFUSE AG and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Planner Dashboard", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ planner/planner/doctype/planner_dashboard/test_planner_dashboard.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ONFUSE AG and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPlannerDashboard(FrappeTestCase):
+	pass
