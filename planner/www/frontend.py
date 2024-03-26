ec385456df903656494a7de2f47a36a7fc8f2144
Christoph Diethelm <c.diethelm@diethelm-aufzuege.swiss>
2024-03-26 16:23:26 +0000

    made everything work in production

---
+++ planner/www/frontend.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+import frappe
+from frappe.utils.telemetry import capture
+
+no_cache = 1
+
+
+def get_context(context):
+	csrf_token = frappe.sessions.get_csrf_token()
+	frappe.db.commit()
+	context.csrf_token = csrf_token
+	return context
