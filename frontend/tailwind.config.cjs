41779a63f06b30462f5f3facaba9ff2eef3e52a6
Christoph Diethelm <c.diethelm@diethelm-aufzuege.swiss>
2024-03-28 07:35:55 +0000

    added tailwind config

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,14 +1,33 @@
+// const
 module.exports = {
-    presets: [
-      require('frappe-ui/src/utils/tailwind.config')
-    ],
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-      "./node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  presets: [require('frappe-ui/src/utils/tailwind.config')],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}',
+    '../node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '2rem',
+        xl: '4rem',
+        '2xl': '4rem',
+      },
     },
-    plugins: [],
-  }
+    extend: {
+      maxWidth: {
+        'main-content': '768px',
+      },
+      screens: {
+        standalone: {
+          raw: '(display-mode: standalone)',
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/container-queries')],
+}
