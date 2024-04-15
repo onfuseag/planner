810c5695d5094576e8a6e63dde0ef0c6ee2a9164
Christoph Diethelm <c.diethelm@diethelm-aufzuege.swiss>
2024-04-15 09:55:44 +0000

    many improvements according to gh repo

--- planner/planner/doctype/project_checklist/project_checklist.js
+++ planner/planner/doctype/project_checklist/project_checklist.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2024, ONFUSE AG and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Project Checklist", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Project Checklist", {
+    refresh(frm) {
+		frm.add_custom_button(__('Email'), function(){
+            const args = {
+    			doc: frm.doc,
+    			frm: frm,
+    			recipients: frm.doc.customer_mail,
+    			is_a_reply: false,
+    			title: null,
+    			last_email: null,
+    			subject: null,
+    			reply_all: false,
+    			attach_document_print: 1
+    		};
+    		new frappe.views.CommunicationComposer(args);
+            
+        });
+	}
+});
