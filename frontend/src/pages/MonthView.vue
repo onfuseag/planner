bf54b36270d3499de8bbe25548e542b280831448
eagleautomate <mn@eagleautomation.in>
2025-08-11 00:16:31 +0530

    change spacing

--- frontend/src/pages/MonthView.vue
+++ frontend/src/pages/MonthView.vue
@@ -4,6 +4,7 @@
     <div class="flex items-center">
       <FeatherIcon name="calendar" class="h-7 w-7 text-gray-500 mr-2.5" />
       <span class="font-semibold text-2xl text-gray-500 mr-2">Planner:</span>
+
       <span class="font-semibold text-2xl">Month View</span>
       <div class="ml-auto space-x-2.5">
         <Button
