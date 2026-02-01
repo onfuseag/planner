7380f8fcb12b6e4af0a8b5d17ade06563cdbaa3b
Mayank Nahata <mayank@Mayanks-Laptop.local>
2026-02-01 21:53:56 +0530

    Upgrade fix: Added department filter

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,4 +1,4 @@
-import { createListResource } from 'frappe-ui'
+import { createListResource, createResource } from 'frappe-ui'
 
 export const projects = createListResource({
   doctype: 'Project',
@@ -68,13 +68,11 @@ export const status = [
   value,
 }))
 
-export const departments = createListResource({
-  doctype: 'Department',
-  fields: ['name', 'department_name'],
+export const departments = createResource({
+  url: 'planner.api.tasks.get_all_departments',
   cache: ['Departments'],
-  pageLength: 99999,
   transform(data) {
-    return data.map((dept) => ({
+    return (data || []).map((dept) => ({
       label: dept.department_name,
       value: dept.name,
     }))
